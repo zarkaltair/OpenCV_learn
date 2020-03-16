a429aba0f5f7612dbe0cdee9ee9af36a134c1424
zarkaltair <zarkaltair@gmail.com>
2020-03-16 22:26:41 +0500

    little fix

--- Lesson_16_motion_detection/motion_detection_and_tracking.py
+++ Lesson_16_motion_detection/motion_detection_and_tracking.py
@@ -2,11 +2,13 @@ import cv2
 import numpy as np
 
 
+# read video file
 cap = cv2.VideoCapture('vtest.avi')
 
 ret, frame1 = cap.read()
 ret, frame2 = cap.read()
 
+# loop over frames of the video
 while cap.isOpened():
     diff = cv2.absdiff(frame1, frame2)
     gray = cv2.cvtColor(diff, cv2.COLOR_BGR2GRAY)
@@ -15,6 +17,7 @@ while cap.isOpened():
     dilated = cv2.dilate(thresh, None, iterations=3)
     contours, _ = cv2.findContours(dilated, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
+    # loop over the contours
     for contour in contours:
         (x, y, w, h) = cv2.boundingRect(contour)
         if cv2.contourArea(contour) < 900:
@@ -22,12 +25,15 @@ while cap.isOpened():
         cv2.rectangle(frame1, (x, y), (x+w, y+h), (0, 255, 0), 2)
         cv2.putText(frame1, 'Status: Movement', (10, 20), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
+    # show the frame
     cv2.imshow('feed', frame1)
     frame1 = frame2
     ret, frame2 = cap.read()
 
+    # if the `Esc` key is pressed, break from the lop
     if cv2.waitKey(40) == 27:
         break
 
+# cleanup the camera and close any open windows
 cv2.destroyAllWindows()
 cap.release()
